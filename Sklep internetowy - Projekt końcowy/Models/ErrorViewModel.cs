2240834443aa6b48a4735a19517116f4bd3389ce
Damian Mitka <damianmitka02@gmail.com>
2023-12-31 13:54:16 +0100

    Dodaj pliki projektów.

---
+++ Sklep internetowy - Projekt końcowy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sklep_internetowy___Projekt_końcowy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
